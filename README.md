1910e54bf29491e4de7c92ab6aeb3d61eff7a00e
Christopher Cifuentes <christophercifuentes.everis@latam.com>
2024-10-23 17:42:56 -0300

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Angular16app
+# elweon
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.16.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular16app'`, () => {
+  it(`should have as title 'elweon'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular16app');
+    expect(app.title).toEqual('elweon');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular16app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('elweon app is running!');
   });
 });
